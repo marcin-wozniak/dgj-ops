4c772f648a97962dd32f9d8559384c089885d05d
root <root@localhost.localdomain>
2022-08-04 16:06:53 +0200

    closing day

--- docker/domibus-gateway-tomcat-5.X/build.sh
+++ docker/domibus-gateway-tomcat-5.X/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-#wget https://search.maven.org/remotecontent?filepath=mysql/mysql-connector-java/8.0.30/mysql-connector-java-8.0.30.jar ./mysql-connector-java.jar
-#wget https://dlm.mariadb.com/2338690/Connectors/java/connector-java-3.0.6/mariadb-java-client-3.0.6.jar ./mariadb-connector-j.jar
+#Download artifacts
+#wget https://search.maven.org/remotecontent?filepath=mysql/mysql-connector-java/8.0.30/mysql-connector-java-8.0.30.jar ./artifacts/mysql-connector-java.jar
+#wget https://dlm.mariadb.com/2338690/Connectors/java/connector-java-3.0.6/mariadb-java-client-3.0.6.jar ./artifacts/mariadb-connector-j.jar
+#wget https://download.oracle.com/otn-pub/otn_software/jdbc/211/ojdbc8.jar ./artifacts/ojdbc8.jar
 docker build -t domibus-gateway-tomcat:5.0 .
